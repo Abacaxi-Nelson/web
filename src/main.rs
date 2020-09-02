2b09530c21a84f43ead9808d2433c237cd0a35ce
Nunes Nelson <nunes.nelson4@gmail.com>
2020-09-02 10:20:38 +0200

    Multithread

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
+use std::io::prelude::*;
 use std::net::TcpListener;
+use std::net::TcpStream;
+use std::thread;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:1234").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        println!("Connection established!");
+        thread::spawn(|| {
+            handle_connection(stream);
+        });
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
